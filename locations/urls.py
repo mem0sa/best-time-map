245586dde350bec1a5c58a0fa5d06a71a3358d3b
mem0sa <maksim.r.2007@mail.ru>
2026-01-12 23:18:20 +0500

    add map click handling and ajax api endpoint

--- locations/urls.py
+++ locations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import map_view
+from .views import map_view, location_click_api
 
 urlpatterns = [
     path('', map_view, name='map'),
+    path('api/location/', location_click_api, name='location_api'),
 ]
--- locations/views.py
+++ locations/views.py
@@ -1,4 +1,27 @@
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+
 
 def map_view(request):
     return render(request, 'locations/map.html')
+
+
+@csrf_exempt
+def location_click_api(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        lat = data.get('lat')
+        lon = data.get('lon')
+
+        print(f'Получены координаты: {lat}, {lon}')
+
+        return JsonResponse({
+            'status': 'ok',
+            'lat': lat,
+            'lon': lon
+        })
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
