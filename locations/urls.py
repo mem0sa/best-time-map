620c66214af5a08487fbaf37b1c23586b3471315
mem0sa <maksim.r.2007@mail.ru>
2026-01-18 18:42:36 +0500

    polish file structure

--- locations/urls.py
+++ locations/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import map_view, location_click_api, place_details_api
+from .views import map_view, place_details_api
 
 urlpatterns = [
     path('', map_view),
-    path('api/location/', location_click_api),
     path('api/place-details/', place_details_api),
 ]
--- locations/views.py
+++ locations/views.py
@@ -2,7 +2,6 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-from .services.google_places import search_organizations
 from .services.google_place_details import get_place_details
 from .services.visit_recommendation import get_visit_recommendation
 from django.conf import settings
@@ -11,23 +10,6 @@ def map_view(request):
     return render(request, 'locations/map.html', {
         'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY
     })
-
-@csrf_exempt
-def location_click_api(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-
-        lat = data.get('lat')
-        lon = data.get('lon')
-
-        organizations = search_organizations(lat, lon)
-
-        return JsonResponse({
-            'status': 'ok',
-            'organizations': organizations
-        })
-
-    return JsonResponse({'error': 'Invalid request'}, status=400)
 
 @csrf_exempt
 def place_details_api(request):
